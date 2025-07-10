1fe6ab5bd973f5916a391f6ed4c0a219250f6a83
Shubham Singh <mine.shubhamsingh@gmail.com>
2025-07-10 21:55:39 +0530

    New Frontend

--- apps/nodezink-fe/app/signup/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthPage } from "@/components/AuthPage";
-
-export default function Signup() {
-  return <AuthPage isSignedIn={false} />;
-}
--- apps/web/app/config.ts
+++
@@ -1,2 +0,0 @@
-export const BACKEND_URL = "http://localhost:8000/api/v1";
-export const WS_URL = "ws://localhost:8080";
